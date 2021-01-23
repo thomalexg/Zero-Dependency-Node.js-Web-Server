52b19fd6672f2a3869301aebdce29bf5122e0da7
thomalexg <thomas.gae@posteo.de>
2021-01-23 17:04:53 +0100

    Done, made a new repository because the old one was fucked up

---
+++ index.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/index.html' || req.url === '/') {
+    res.write(fs.readFileSync('./public/index.html'));
+    res.end();
+  } else if (req.url === '/index.css') {
+    res.write(fs.readFileSync('./public/index.css'));
+    res.end();
+  } else if (req.url === '/memes' || req.url === '/memes/index.html') {
+    res.write(fs.readFileSync('./public/memes/index.html'));
+    res.end();
+  }
+  // else if (req.url === '/1.jpg') {
+  //   fs.readFile('./memes/1.jpg', (error, data) => {
+  //     if (error) throw error;
+  //     res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+  //     res.write(data);
+  //     res.end();
+  //   });
+  // }
+  else if (req.url === '/1.jpg') {
+    res.writeHead(200, { 'Content-Type': 'image/jpg' });
+    res.end(fs.readFileSync('./public/memes/1.jpg'));
+  } else {
+    res.write(JSON.stringify(404));
+    res.end();
+  }
+});
+
+server.listen(3000);
+
+console.log('server running on port 3000...');
