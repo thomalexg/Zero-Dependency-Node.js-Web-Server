46ccacb6242e8643bd6a12016db8afb0292fe1dd
thomalexg <thomas.gae@posteo.de>
2021-04-06 14:42:39 +0200

    Removed unnecessary code

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const http = require('http');
 const fs = require('fs');
 
-function resFunc(res, request) {
-  res.write(fs.readFileSync(`./public/${request}`));
-  res.end();
-}
-function resFuncIndex(res, request) {
-  res.write(fs.readFileSync(`./public/${request}/index.html`));
-  res.end();
-}
 function resFailed(res) {
   res.statusCode = 404;
   res.write(JSON.stringify(404));
@@ -22,13 +14,11 @@ const server = http.createServer((req, res) => {
       !fs.lstatSync(`./public${request}`).isDirectory() &&
       fs.existsSync(`./public${request}`)
     ) {
-      if (request === '/') {
-        resFuncIndex(res, request);
-      } else {
-        resFunc(res, request);
-      }
+      res.write(fs.readFileSync(`./public${request}`));
+      res.end();
     } else if (fs.lstatSync(`./public${request}`).isDirectory()) {
-      resFuncIndex(res, request);
+      res.write(fs.readFileSync(`./public${request}/index.html`));
+      res.end();
     } else {
       resFailed(res);
     }
@@ -40,13 +30,3 @@ const server = http.createServer((req, res) => {
 server.listen(3000);
 
 console.log('server running on port 3000...');
-
-// other way of writing the esle if:
-// else if (req.url === '/1.jpg') {
-//   fs.readFile('./memes/1.jpg', (error, data) => {
-//     if (error) throw error;
-//     res.writeHead(200, { 'Content-Type': 'image/jpeg' });
-//     res.write(data);
-//     res.end();
-//   });
-// }
